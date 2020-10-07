55ff04a51ae553a796f7d4e22b616220d032e8aa
Max Ortega <mortegar15_2@unc.edu.pe>
2020-10-07 12:48:41 -0500

    commit

--- practicanoviembre/script/ortega.js
+++ practicanoviembre/script/ortega.js
@@ -1,26 +1,77 @@
-const lc = document.getElementById("lc1");
-lc.addEventListener('click', function() {
-    document.getElementById("text1").value = 'Lectura completada';
+const lc1 = document.getElementById("lc1");
+lc1.addEventListener('click', function() {
+    let text1 = document.getElementById("text1");
+    if (text1.value == 'Lectura de 3 min') {
+        document.getElementById("text1").value = 'Lectura completada';
+    } else {
+        document.getElementById("text1").value = 'Lectura de 3 min';
+    }
+
 })
 
 
 
-
-const lc = document.getElementById("lc2");
-lc.addEventListener('click', function() {
-    document.getElementById("text2").value = 'Lectura completada';
+const lc2 = document.getElementById("lc2");
+lc2.addEventListener('click', function() {
+    let text2 = document.getElementById("text2");
+    if (text2.value == 'Lectura de 2 min') {
+        document.getElementById("text2").value = 'Lectura completada';
+    } else {
+        document.getElementById("text2").value = 'Lectura de 2 min';
+    }
 })
 
 
 
-const lc = document.getElementById("lc3");
-lc.addEventListener('click', function() {
-    document.getElementById("text3").value = 'Lectura completada';
+const lc3 = document.getElementById("lc3");
+lc3.addEventListener('click', function() {
+    let text3 = document.getElementById("text3");
+    if (text3.value == 'Lectura de 3 min') {
+        document.getElementById("text3").value = 'Lectura completada';
+    } else {
+        document.getElementById("text3").value = 'Lectura de 3 min';
+    }
 })
 
 
 
-const lc = document.getElementById("lc4");
-lc.addEventListener('click', function() {
-    document.getElementById("text4").value = 'Lectura completada';
+const lc4 = document.getElementById("lc4");
+lc4.addEventListener('click', function() {
+    let text4 = document.getElementById("text4");
+    if (text4.value == 'Lectura de 5 min') {
+        document.getElementById("text4").value = 'Lectura completada';
+    } else {
+        document.getElementById("text4").value = 'Lectura de 5 min';
+    }
+})
+
+
+
+const mostfec = document.getElementById("mostfec");
+let f = new Date();
+mostfec.addEventListener('click', function() {
+    let text5 = document.getElementById("text5");
+    if (text5.value == '') {
+        document.getElementById("text5").value = f;
+
+    } else {
+        document.getElementById("text5").value = '';
+
+    }
+})
+
+
+
+const mostpan = document.getElementById("mostpan");
+mostpan.addEventListener('click', function() {
+    let fotos = document.getElementById("fotos");
+    if (fotos.style.display == "block") {
+        document.getElementById("turismo").style.display = "block";
+        document.getElementById("fotos").style.display = "none";
+
+    } else {
+        document.getElementById("turismo").style.display = "none";
+        document.getElementById("fotos").style.display = "block";
+
+    }
 })
