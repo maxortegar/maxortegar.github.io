51af1cdb7d883ee8c95f1e5c0b552479d5786c84
Max Ortega <mortegar15_2@unc.edu.pe>
2020-10-07 09:31:33 -0500

    commit

---
+++ practicanoviembre/script/ortega.js
@@ -0,0 +1,26 @@
+const lc = document.getElementById("lc1");
+lc.addEventListener('click', function() {
+    document.getElementById("text1").value = 'Lectura completada';
+})
+
+
+
+
+const lc = document.getElementById("lc2");
+lc.addEventListener('click', function() {
+    document.getElementById("text2").value = 'Lectura completada';
+})
+
+
+
+const lc = document.getElementById("lc3");
+lc.addEventListener('click', function() {
+    document.getElementById("text3").value = 'Lectura completada';
+})
+
+
+
+const lc = document.getElementById("lc4");
+lc.addEventListener('click', function() {
+    document.getElementById("text4").value = 'Lectura completada';
+})
